12cc2a58d75b6fd332bb678fb823cc46cc59a1e8
Benedettainnocenti <innocentibenedetta@gmail.com>
2025-12-11 22:12:50 +0100

    tentativo creazione chiavi con problemi

--- etl_sondaggio.sql
+++ etl_sondaggio.sql
@@ -61,7 +61,7 @@ order by vittima_o_testimone_di_discriminazione_in_azienda;
 
 --dim tipo discriminazione
 
-drop table if exists dim_tipo_discriminazione;
+drop table if exists dim_tipo_discriminazione CASCADE;
 create table if not exists dim_tipo_discriminazione as
  select row_number() over() as ids_tipo_discriminazione, * from (
 SELECT DISTINCT
@@ -71,7 +71,7 @@ LEFT JOIN LATERAL unnest(string_to_array(pa.tipo_discriminazione, ',')) AS elem 
 
 -- qui sotto la query che funziona con i dati puliti della dim_tipo_violenza
 
-drop table if exists dim_tipo_violenza;
+drop table if exists dim_tipo_violenza cascade;
 create table if not exists dim_tipo_violenza as
  select row_number() over() as ids_tipo_violenza, * from (
 SELECT DISTINCT
@@ -107,7 +107,7 @@ create table dim_presenza_regolamenti_antidiscriminazione as
 --- tabella manuale per andare a correggere valori delle province
 create table if not exists sondaggio_transformation.man_mapping_province ( originale text,corretto text);
 
-drop table if exists sondaggio_transformation.tt_provincia_validated_v01;
+drop table if exists sondaggio_transformation.tt_provincia_validated_v01 cascade;
 
 
 --- cerca province nella tabella istat con tutte le province
@@ -135,7 +135,7 @@ order by provincia asc;
 --- MANCA DA COLLEGARE LA DIM_REGIONE
 
 
-drop table if exists sondaggio_transformation.dim_provincia;
+drop table if exists sondaggio_transformation.dim_provincia cascade;
 create table sondaggio_transformation.dim_provincia as 
 select row_number() over( order by provincia asc) as ids_provincia, provincia
 from
@@ -158,11 +158,11 @@ union all
 select -1 as ids_provincia,'Provincia sconosciuta' as provincia
 ;
 
-drop table if exists sondaggio_transformation.et_dim_provincia;
+drop table if exists sondaggio_transformation.et_dim_provincia cascade;
 
 
 -- tabella errori provincia
-drop table if exists et_dim_provincia;
+drop table if exists et_dim_provincia cascade;
 create table if not exists et_dim_provincia as 
 select FORMAT('Valore provincia "%s" non valido',rotti.provincia ) as messaggio from(
 select provincia 
@@ -177,7 +177,7 @@ where mapping.corretto is null
 
 -- toglie spazi da campi provincia domicilio e ultimo lavoro
 
-drop table if exists sondaggio_transformation.tt_sondaggio_province_trim_v1;
+drop table if exists sondaggio_transformation.tt_sondaggio_province_trim_v1 cascade;
 create table sondaggio_transformation.tt_sondaggio_province_trim_v1 as
 select "timestamp", genere, fascia_eta, trim(provincia_domicilio) as provincia_domicilio, grandezza_azienda, durata_lavoro_in_azienda, trim(provincia_ultimo_lavoro) as provincia_ultimo_lavoro ,vittima_o_testimone_di_discriminazione_in_azienda, tipo_discriminazione, vittima_o_testimone_di_violenza, tipo_violenza, presenza_formazione_antidiscriminazione_in_azienda, presenza_regolamenti_antidiscriminazione from 
 sondaggio.progetto_andromeda pa;
@@ -186,7 +186,7 @@ sondaggio.progetto_andromeda pa;
 -- trasformazione di tutte le province domicilio nella versione estesa 
 -- se sono delle sigle (se sono rotte rimangono rotte)
 
-drop table if exists sondaggio_transformation.tt_sondaggio_province_domicilio_ok_v2;
+drop table if exists sondaggio_transformation.tt_sondaggio_province_domicilio_ok_v2 cascade;
 create table sondaggio_transformation.tt_sondaggio_province_domicilio_ok_v2 as
 select "timestamp", genere, fascia_eta, t."Provincia" as provincia_domicilio, grandezza_azienda, durata_lavoro_in_azienda,  provincia_ultimo_lavoro, vittima_o_testimone_di_discriminazione_in_azienda, tipo_discriminazione, vittima_o_testimone_di_violenza, tipo_violenza, presenza_formazione_antidiscriminazione_in_azienda, presenza_regolamenti_antidiscriminazione from 
 sondaggio_transformation.tt_sondaggio_province_trim_v1 pa
@@ -203,7 +203,7 @@ where t."Provincia" is null;
 -- trasformazione di tutte le province ultimo lavoro nella versione estesa 
 -- se sono delle sigle (se sono rotte rimangono rotte)
 
-drop table if exists sondaggio_transformation.tt_sondaggio_province_ultimo_lavoro_ok_v3;
+drop table if exists sondaggio_transformation.tt_sondaggio_province_ultimo_lavoro_ok_v3 cascade;
 create table sondaggio_transformation.tt_sondaggio_province_ultimo_lavoro_ok_v3 as
 select "timestamp", genere, fascia_eta, provincia_domicilio, grandezza_azienda, durata_lavoro_in_azienda,  t."Provincia" as provincia_ultimo_lavoro, vittima_o_testimone_di_discriminazione_in_azienda, tipo_discriminazione, vittima_o_testimone_di_violenza, tipo_violenza, presenza_formazione_antidiscriminazione_in_azienda, presenza_regolamenti_antidiscriminazione from 
 sondaggio_transformation.tt_sondaggio_province_domicilio_ok_v2 pa
@@ -217,7 +217,7 @@ on lower(trim(pa.provincia_ultimo_lavoro))=lower(t."Sigla")
 where t."Provincia" is null;
 
 --creazione del fatto 
-drop table if exists fact_sondaggio;
+drop table if exists fact_sondaggio cascade;
 create table if not exists fact_sondaggio as 
 select 
     row_number() over() as ids_risposta,
@@ -270,3 +270,88 @@ left join sondaggio_transformation.dim_presenza_regolamenti_antidiscriminazione 
         on dpra.presenza_regolamenti_antidiscriminazione = 
            pa.presenza_regolamenti_antidiscriminazione 
 ) t;
+
+
+--CREAZIONE DELLE CHIAVI 
+ALTER TABLE dim_genere
+ADD CONSTRAINT dim_genere2 UNIQUE (ids_genere);
+
+alter table fact_sondaggio
+add constraint fact_2 FOREIGN KEY (ids_genere) references dim_genere (ids_genere);
+
+
+
+ALTER TABLE dim_fascia_eta 
+ADD CONSTRAINT dim_fascia_eta2 UNIQUE (ids_fascia_eta);
+
+alter table fact_sondaggio
+add constraint fact_3 FOREIGN KEY (ids_fascia_eta) references dim_fascia_eta (ids_fascia_eta);
+
+--ERRORE per mancata corrispondenza con fittizio 
+ALTER TABLE dim_provincia_domicilio
+ADD CONSTRAINT dim_provincia2 UNIQUE (ids_provincia_domicilio);
+
+alter table fact_sondaggio
+add constraint fact_4 FOREIGN KEY (ids_provincia_domicilio) references dim_provincia_domicilio (ids_provincia_domicilio);
+
+
+ALTER TABLE dim_grandezza_azienda
+ADD CONSTRAINT dim_grandezza2 UNIQUE (ids_grandezza_azienda);
+
+alter table fact_sondaggio
+add constraint fact_5 FOREIGN KEY (ids_grandezza_azienda) references dim_grandezza_azienda (ids_grandezza_azienda);
+
+--ERRORE PER MANCATA CORRISPONDENZA CON FITTIZIO
+ALTER TABLE dim_durata_lavoro_in_azienda
+ADD CONSTRAINT dim_durata2 UNIQUE (ids_durata_lavoro_in_azienda);
+
+alter table fact_sondaggio
+add constraint fact_ FOREIGN KEY (ids_durata_lavoro_in_azienda) references dim_durata_lavoro_in_azienda (ids_durata_lavoro_in_azienda);
+
+--ERRORE MANCANZA FITTIZIO
+
+ALTER TABLE dim_provincia_ultimo_lavoro
+ADD CONSTRAINT dim_ultimolavoro2 UNIQUE (id);
+
+alter table fact_sondaggio
+add constraint fact8 FOREIGN KEY (ids_provincia_ultimo_lavoro) references dim_provincia_ultimo_lavoro (id);
+
+
+
+ALTER TABLE dim_vittima_o_testimone_di_discriminazione_in_azienda
+ADD CONSTRAINT dim_vittima UNIQUE (ids_vittima_o_testimone_di_discriminazione_in_azienda);
+
+alter table fact_sondaggio
+add constraint fact9 FOREIGN KEY (ids_vittima_o_testimone) references dim_vittima_o_testimone_di_discriminazione_in_azienda (ids_vittima_o_testimone_di_discriminazione_in_azienda);
+
+--MANCATA CORRISPONDENZA CON FITTIZIO
+ALTER TABLE dim_tipo_discriminazione
+ADD CONSTRAINT dim_discriminazione UNIQUE (ids_tipo_discriminazione);
+
+alter table fact_sondaggio
+add constraint fact10 FOREIGN KEY (ids_tipo_discriminazione) references dim_tipo_discriminazione (ids_tipo_discriminazione);
+
+--MANCATA CORRISPONDENZA CON FITTIZIO
+ALTER TABLE dim_tipo_violenza
+ADD CONSTRAINT dim_violenza2 UNIQUE (ids_tipo_violenza);
+
+alter table fact_sondaggio
+add constraint fact11 FOREIGN KEY (ids_tipo_violenza) references dim_tipo_violenza (ids_tipo_violenza);
+
+
+ALTER TABLE dim_presenza_formazione_antidiscriminazione_in_azienda 
+ADD CONSTRAINT dim_presenza_formazione2 UNIQUE (ids_presenza_formazione_antidiscriminazione_in_azienda);
+
+alter table fact_sondaggio
+add constraint fact12 FOREIGN KEY (ids_presenza_formazione) references dim_presenza_formazione_antidiscriminazione_in_azienda (ids_presenza_formazione_antidiscriminazione_in_azienda) ;
+
+--MANCATA CORRISPONDENZA CON FITTIZIO
+ALTER TABLE dim_presenza_regolamenti_antidiscriminazione 
+ADD CONSTRAINT dim_presenza_antidiscrim2 UNIQUE (ids_presenza_regolamenti_antidiscriminazione);
+
+alter table fact_sondaggio
+add constraint fact13 FOREIGN KEY (ids_presenza_regolamenti) references dim_presenza_regolamenti_antidiscriminazione  (ids_presenza_regolamenti_antidiscriminazione) ;
+
+
+
+
